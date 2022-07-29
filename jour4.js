13d19d0ba33df87731914ec09b1b8ac823b75d16
naposimba1 <naposimba.gmail.com>
2022-07-29 20:12:18 +0200

    jour4

---
+++ jour4.js
@@ -0,0 +1,24 @@
+//les tableaux  
+// Un tableau est une variable spéciale, qui peut contenir plusieurs valeurs: Exemple: Let cars =["Saab", "Volvo","BMW"]
+// tableau idufasha kubika ama valeurs menshi. let cars= ("Volvo","BMW");
+// Ukenee kubika ibintu vyinshi de même valeur uca u declara tableau.;
+// let cars= "Volvo","BMw","Saab";
+// Syntaxe pour déclarer un tableau:    let array_name= [] ex: let eleves = ["Napoléon", "Peace", "Prosper"]
+// Dans un tableau,  on compte les indices en commençant par 0
+// let xy = [];
+// xy:length;  //pour compter la taille du tableau
+// xy[10]= "Holla"
+// xy.length;  //donne 11
+
+// let b = [1,2,3,4,5,6,7,8,9,10]
+// b[0];
+
+// b.pop(9)
+// la méthode pop ni io gukuramwo ikintu canyuma muri tableau
+// b.push(45) la méthode push ni io gushiramwo ikintu ca nyuma muri tableau
+
+
+for (let i = 0, i<= 50, i++) {
+    console.log("Hello hogi");
+}
+
