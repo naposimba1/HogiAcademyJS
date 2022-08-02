cca29c63a13f2a957f52d41540684063ffe74439
naposimba1 <naposimba.gmail.com>
2022-08-02 20:08:08 +0200

    Tp

--- jour5.js
+++ jour5.js
@@ -31,7 +31,32 @@ const CNI = {
 };
 //Pour récupérer les valeurs d'un objet, on fait    CNI.NOM et la machine affiche le nom
 
+const CNI = {
+    NOM: "SIMBANDUMWE",
+    PRENOM: "Napoléon",
+    SEXE: "M",
+    NOmPERE: "SIMBANDUMWE SEVERIN",
+    NOMMERE: "NTAKARUTIMANA VIRGINIE",
+    DATENAISSANCE: "1er Juin 1988",
+    LIEU_Naissance: "Bujumbura Marie MUKAZA BUYENZI",
+    //LIEU_Naissance: {id: 24, name:"K"Bujumbura Marie MUKAZA BUYENZI"} 
+    Etat_civil: "Marié",
+    Fonction: "Informaticien",
+    NUMCNI: "0201/324.126 Délivrée le 19/06/2017",
+    fullName: function () {
+        return this.NOM + " " + this.PRENOM + " " + this.DATENAISSANCE;
+    }
+    // on peut utiliser aussi CNI à la place de this  EX:  fullName: function () {
+    //     return CNI.NOM + " " + this.PRENOM + " " +this.DATENAISSANCE;   // on peut utiliser aussi CNI à la place de this  EX: 
+    // }
+
+};
+// Expliquer while et do while
+//2.TP: utiliser la boucle FOR ... IN pour afficher toutes les valeurs qui sont dans l'objet CNI
+//3. Savoir comment utiliser la boucle  FOR OFF
 
 
 
+
+//Pour afficher les valeurs d'une fonction, on fait CNI.fullname()
 
