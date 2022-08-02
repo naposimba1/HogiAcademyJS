57b64c182ac3768dc1ec07695ecac349c9288cbc
naposimba1 <naposimba.gmail.com>
2022-08-02 19:29:05 +0200

    jour5

---
+++ jour5.js
@@ -0,0 +1,34 @@
+let i = 10
+do {
+    document.write("Peace and love");
+    i++
+} while (i < 11)
+
+
+// for (var i = 5; i != 0; i -= 2) {
+//     document.write("bonjour");
+// }
+
+var x = 5;
+while (x == 5) {
+    console.log("Yambu");
+}
+
+
+
+// objet doit avoir une clé et une valeur
+const CNI = {
+    NOm: "SIMBANDUMWE",
+    PRENOM: "Napoléon",
+    SEXE: "M",
+    NOmPERE: "SIMBANDUMWE SEVERIN",
+    NOMMERE: "NTAKARUTIMANA VIRGINIE",
+    DATENAISSANCE: "1er Juin 1988",
+    LIEU_Naissance: "Bujumbura Marie MUKAZA BUYENZI",
+    Etat_civil: "Marié",
+    Fonction: "Informaticien",
+    NUMCNI: "0201/324.126 Délivrée le 19/06/2017"
+};
+
+
+
