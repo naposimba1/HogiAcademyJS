e8cc0a6dd89a41c4ba66b0725f3cc197bdff4a01
naposimba1 <naposimba.gmail.com>
2022-08-05 16:45:57 +0200

    Quiz

---
+++ exo/app.js
@@ -0,0 +1,58 @@
+let nbQuestions = 3;
+console.log(nbQuestions);
+let quizBegin = true;
+console.log(quizBegin);
+alert("Bienvenu sur ce quiz! " + " \n " + nbQuestions + "3 questions seront posées");
+
+let result = prompt(`Question 1/3
+Quel est l'âge du capitaine?
+1. 42
+2. 101
+3. 18`);
+let trueResult = 1;
+console.log(result);
+
+if (result == trueResult) {
+    console.log("T'as gagné");
+}
+else {
+    console.log("T'as échoué!!");
+}
+
+//
+
+
+let result = prompt(`Question 2/3
+Quel est la capitale de France?
+1. Londres
+2. Paris
+3. Berlin`);
+let trueResult = 1;
+console.log(result);
+
+if (result == trueResult) {
+    console.log("T'as gagné");
+}
+else {
+    console.log("T'as échoué!!");
+}
+
+
+
+
+let result = prompt(`Question 3/3
+Qu'est ce qui est jaune et qui attend'?
+1. Jonathan
+2. Homer Simpson
+3. Un citron pressé`);
+let trueResult = 1;
+console.log(result);
+
+if (result == trueResult) {
+    console.log("T'as gagné");
+}
+else {
+    console.log("T'as échoué!!");
+}
+
+
--- explication.js
+++ explication.js
@@ -8,6 +8,13 @@ while (i < 30) {
 }
 
 
+let i = 0;
+do {
+    console.log("HELLO HOGI");
+}
+
+while (i < 30)
+i++;
 
 
 // on peut utiliser la boucle for
@@ -22,28 +29,28 @@ const employes = [{
     prenom: "Alexis",
     age: "38",
     service: "Statisticien",
-    salaire: "500000"
+    salaire: 500000
 },
 {
     nom: "BUKURU",
     prenom: "Anicet",
     age: "36",
     service: "Informaticien",
-    salaire: "600000"
+    salaire: 600000
 },
 {
     nom: "HAKIZIMANA",
     prenom: "Ange",
     age: "25",
     service: "Economiste",
-    salaire: "350000"
+    salaire: 350000
 },
 {
     nom: "KARENZO",
     prenom: "PAUL",
     age: "28",
     service: "Economiste",
-    salaire: "360000"
+    salaire: 360000
 }
 
 ];
@@ -54,7 +61,7 @@ employes.forEach(element => {
 
 
 let AjoutSalaire = employes.map(element => {
-    return element.salaire * 2;
+    return element.salaire + 50000;
 })
 console.log(AjoutSalaire);
 
@@ -71,3 +78,38 @@ console.log(filtreSalaire);
 
 
 
+<script>
+    var min= Number.MIN_VALUE;
+    var max= Number.MAX_VALUE;
+    var infneg= Number.NEGATIVE_INFINITY;
+    var infpos= Number.POSITIVE_INFINITY;
+    var notnumber= Number.NaN;
+
+    {/* alert ('Min : ' + min +
+    '\nMax': + max +
+    '\nInfini négatif:' + infneg +
+    '\nInfini positif : ' + infpos +
+    '\nPAs un nombre:' + notnumber +
+    ); */}
+    console.log("Min :" + min, "Max :" + max, "Infini négatif :"+ infneg, "Infini positif :" + infneg, "Infini positif :" +infpos, "Pas un nombre :" +notnumber);
+
+
+    var prenom = ['Pierre', 'Victor', 'Julia', 'Claire'], p='';
+    {/* alert (prenom[0]); */}
+    console.log("Notre tableau contient :" + prenom.length
+    + "éléments");
+    var taille= = prenompush('Napoléon','Pivoine');
+
+    for (var i=0; i<prenom.length; i++){
+        p += 'Prénom n°' + (i + 1) + ':' + prenom[i] + '\n'; 
+    }
+
+    console.log("Le tableau contient :" + taille + "éléments");
+    for (var cleffs in prenom){
+        p += clefs + ':' + prenom[clefs] + '\n';
+    }
+    alert(p);
+
+
+
+</script>
